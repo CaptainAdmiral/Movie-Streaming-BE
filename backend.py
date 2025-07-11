38b4b35ac798fdd3f5154f1487111b93caaa411e
Matt <mattb1197@live.co.uk>
2025-07-11 13:57:29 +0100

    Update render CORS with new target

--- backend.py
+++ backend.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://my-react-app.onrender.com"],
+    allow_origins=["http://localhost:3000", "https://movie-streaming-fe-6xdw.onrender.com"],
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
